47bdae60c2e7e2f784645b0341172a1e640ec675
Saulson <saulson.96@gmail.com>
2021-05-08 16:27:31 -0500

    Se agregan botones para iniciar y parar los caballos

--- FCFS/Form1.Designer.cs
+++ FCFS/Form1.Designer.cs
@@ -33,7 +33,8 @@ namespace FCFS
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.pictureBoxBackground = new System.Windows.Forms.PictureBox();
-            // this.pictureBoxesHorses = new List<System.Windows.Forms.PictureBox>();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.buttonStop = new System.Windows.Forms.Button();
             this.pictureBoxesHorse = new System.Collections.Generic.List<System.Windows.Forms.PictureBox>();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBackground)).BeginInit();
             this.SuspendLayout();
@@ -57,24 +58,50 @@ namespace FCFS
                 ((System.ComponentModel.ISupportInitialize)(pictureBoxHorse)).BeginInit();
                 pictureBoxHorse.Image = ((System.Drawing.Image)(resources.GetObject("Horse")));
                 pictureBoxHorse.Location = new System.Drawing.Point(35, 154 + (80 * i));
-                pictureBoxHorse.Name = "Horse" + (i + 1).ToString();
+                pictureBoxHorse.Name = "pictureBoxHorse" + (i + 1).ToString();
                 pictureBoxHorse.Size = new System.Drawing.Size(70, 35);
                 pictureBoxHorse.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-                pictureBoxHorse.TabIndex = i + 1;
+                // pictureBoxHorse.TabIndex = i + 1;
                 pictureBoxHorse.TabStop = false;
                 ((System.ComponentModel.ISupportInitialize)(pictureBoxHorse)).EndInit();
                 this.Controls.Add(pictureBoxHorse);
                 this.pictureBoxesHorse.Add(pictureBoxHorse);
             }
+
+            //
+            // buttonStart
+            //
+            this.buttonStart.Location = new System.Drawing.Point(100, 500);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 23);
+            this.buttonStart.TabIndex = 2;
+            this.buttonStart.Text = "Iniciar";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.ButtonStartClick);
+
+            //
+            // buttonStop
+            //
+            this.buttonStop.Location = new System.Drawing.Point(200, 500);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(75, 23);
+            this.buttonStop.TabIndex = 2;
+            this.buttonStop.Text = "Parar";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.ButtonStopClick);
+
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 550);
             this.Controls.Add(this.pictureBoxBackground);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.buttonStop);
             this.Name = "FCFS";
             this.Text = "FCFS";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.ActionFormClosing);
 
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBackground)).EndInit();
             // ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
@@ -87,6 +114,8 @@ namespace FCFS
 
         #endregion
 
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Button buttonStop;
         private System.Windows.Forms.PictureBox pictureBoxBackground;
         private System.Collections.Generic.List<System.Windows.Forms.PictureBox> pictureBoxesHorse;
     }
