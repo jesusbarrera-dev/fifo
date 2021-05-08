5c70a35e7792699bceca7af6e5a8a40f23b9b3be
Jesús Barrera <jesusbarreradev@gmail.com>
2021-05-08 12:57:18 -0500

    clases threadWork y threadTest agregadas

--- FCFS/Program.cs
+++ FCFS/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace FCFS
 {
@@ -17,6 +18,28 @@ namespace FCFS
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+        }
+    }
+    public class ThreadWork
+        {
+            public static void DoWork()
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    Console.WriteLine("Working thread..."); Thread.Sleep(100);
+                }
+            }
+        }
+    class ThreadTest
+    {
+        public static void Main()
+        {
+            Thread thread1 = new Thread(ThreadWork.DoWork); thread1.Start();
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("In main.");
+                Thread.Sleep(100);
+            }
         }
     }
 }
