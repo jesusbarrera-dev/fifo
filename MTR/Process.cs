3d24dee1ed461d8807fda565f148e52d6a66c001
Jesús Barrera <jesusbarreradev@gmail.com>
2021-05-24 17:07:20 -0500

    Practica Menor Tiempo Posible - commit inicial

---
+++ MTR/Process.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica
+{
+    class Process
+    {
+        public int ID { get; set; }
+        public int ArrivalTime { get; set; }
+        public int ExecutionTime { get; set; }
+        public int ExecutedTime { get; set; }
+    }
+}
