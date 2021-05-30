fa3f44db894d194566606077e00067846159161a
Saulson <saulson.96@gmail.com>
2021-05-30 18:06:12 -0500

    Se agrega proyecto para la practica MLQ

---
+++ MLQ/Form1.cs
@@ -0,0 +1,304 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Practica
+{
+    public partial class Form1 : Form
+    {
+        private const int UNIT_TIME = 100; // 100ms
+        private const int MAX_PROCESSES = 20;
+        private const int MAX_EXECUTION_TIME = 21;
+        private const int MAX_SCHEDULE_TIME = 15;
+
+        private Random rand;
+        private int actualTime = 0;
+        private int idleTime = 0;
+
+        private Thread threadProcesses;
+        private Queue<Process> processes;
+
+        private List<int> responseTimes;
+        private List<int> turnaroundTimes;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.rand = new Random();
+            this.processes = new Queue<Process>();
+            this.responseTimes = new List<int>();
+            this.turnaroundTimes = new List<int>();
+
+            this.GenerateProcesses();
+        }
+
+        private void GenerateProcesses()
+        {
+            Process p;
+            Series s;
+
+            this.processes.Clear();
+            this.chart1.Series.Clear();
+
+            for (int i = 0; i < MAX_PROCESSES; i++)
+            {
+                p = new Process()
+                {
+                    ID = i + 1,
+                    ExecutionTime = rand.Next(1, MAX_EXECUTION_TIME) * UNIT_TIME, //Tiempo de ejecución entre 100 y 2000 ms
+                };
+
+                this.processes.Enqueue(p);
+
+                s = this.chart1.Series.Add(p.ID.ToString());
+                s.Color = Color.White;
+                s.BorderColor = Color.Black;
+                s.Points.Add(p.ExecutionTime);
+                s.Label = p.ID.ToString();
+            }
+        }
+
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            this.threadProcesses = new Thread(new ThreadStart(this.RunSRT));
+            this.threadProcesses.Start();
+
+            this.btnStart.Enabled = false;
+        }
+
+        // Al cerrar la ventana se abrotará el hilo
+        public void ActionFormClosing(object sender, FormClosingEventArgs e)
+        {
+            this.KillProcesses();
+        }
+
+        private void KillProcesses()
+        {
+            if (this.threadProcesses != null)
+            {
+                this.threadProcesses.Abort();
+                this.threadProcesses = null;
+            }
+        }
+
+        private void RunSRT()
+        {
+            List<Process> actualProcesses = new List<Process>();
+            Process p;
+
+            UpdateProcessInChartCallback cbChart = new UpdateProcessInChartCallback(this.UpdateProcessInChart);
+            UpdateResponseTimeCallback cbResponse = new UpdateResponseTimeCallback(this.UpdateResponseTime);
+            UpdateTurnaroundTimeCallback cbTurnaround = new UpdateTurnaroundTimeCallback(this.UpdateTurnaroundTime);
+            UpdateCpuTimeCallback cbCPU = new UpdateCpuTimeCallback(this.UpdateCpuTime);
+
+            this.actualTime = 0;
+            this.idleTime = 0;
+
+            int i;
+            // Tiempo en que se encolará nuevo proceso entre 100 a 1400 ms
+            int timeToNewProcess = actualTime + rand.Next(1, MAX_SCHEDULE_TIME) * UNIT_TIME;
+
+            while (this.processes.Count > 0 || actualProcesses.Count > 0)
+            {
+                p = actualProcesses.Count > 0
+                    ? actualProcesses[0]
+                    : null;
+
+                if (p != null) // Proceso ejecutandose
+                {
+                    if (p.ExecutedTime == 0) //Calculo tiempo de respuesta (TiempoPrimeraEjecucion - TiempoLlegada)
+                    {
+                        this.Invoke(cbResponse, this.actualTime - p.ArrivalTime);
+                    }
+
+                    this.Invoke(cbChart, p, Color.Red);
+                }
+
+                Thread.Sleep(UNIT_TIME);
+                this.actualTime += UNIT_TIME;
+
+                if (p != null)
+                {
+                    p.ExecutedTime += UNIT_TIME; // Actualización del tiempo de ejecución del proceso actual
+
+                    if (p.ExecutedTime == p.ExecutionTime) // Proceso Terminado
+                    {
+                        p = actualProcesses[0];
+                        actualProcesses.RemoveAt(0);
+                        this.Invoke(cbChart, p, Color.White);
+
+                        // Calculo Tiempo Tournaround (TiempoTerminaEjecucion - TiempoLLegada)
+                        this.Invoke(cbTurnaround, this.actualTime - p.ArrivalTime);
+                    }
+                }
+                else // No hubo ningun proceso en la cola de ejecución
+                {
+                    this.idleTime += UNIT_TIME;
+                }
+
+                this.Invoke(cbCPU);
+
+                if (this.actualTime == timeToNewProcess) // Llega nuevo proceso
+                {
+                    if (this.processes.Count > 0)
+                    {
+                        p = this.processes.Dequeue();
+                        p.ArrivalTime = this.actualTime;
+                        this.Invoke(cbChart, p, Color.MidnightBlue);
+
+                        for (i = 0; i < actualProcesses.Count; i++)
+                        {
+                            if (p.RemaingTime < actualProcesses[i].RemaingTime)
+                            {
+                                if (i == 0) // El proceso actual será bloqueado porqué llego un proceso con menor tiempo
+                                {
+                                    this.Invoke(cbChart, actualProcesses[0], Color.DarkGreen);
+                                }
+                                break;
+                            }
+                        }
+
+                        actualProcesses.Insert(i, p); // Se inserta ordenado por el tiempo restante de ejecución en la lista de procesos
+
+                        // Tiempo en que se insertará nuevo proceso entre 100 a 1400 ms
+                        timeToNewProcess = actualTime + rand.Next(1, MAX_SCHEDULE_TIME) * UNIT_TIME;
+                    }
+                }
+            }
+        }
+
+
+        private void UpdateProcessInChart(Process p, Color c)
+        {
+            Series s = this.chart1.Series.FindByName(p.ID.ToString());
+            if (p.ExecutionTime == p.ExecutedTime)
+            {
+                this.chart1.Series.Remove(s);
+                return;
+            }
+
+            s.Color = c;
+            s.Points[0].SetValueY(p.RemaingTime);
+        }
+
+        private void UpdateResponseTime(int responseTime)
+        {
+            this.responseTimes.Add(responseTime);
+
+            int minTime = responseTime;
+            int avgTime = 0;
+            int desvTime = 0;
+            int maxTime = responseTime;
+
+            foreach (int time in this.responseTimes)
+            {
+                avgTime += time;
+
+                if (time < minTime)
+                {
+                    minTime = time;
+                }
+
+                if (time > maxTime)
+                {
+                    maxTime = time;
+                }
+            }
+
+            avgTime /= this.responseTimes.Count;
+            foreach (int time in this.responseTimes)
+            {
+                desvTime += (int)Math.Pow(time - avgTime, 2);
+            }
+            desvTime = (int)Math.Sqrt(desvTime / this.responseTimes.Count);
+
+            // Actualización de la interfaz
+            this.rtMinLabel.Text = minTime.ToString() + " ms";
+            this.rtAvgLabel.Text = avgTime.ToString() + " ms";
+            this.rtDesvLabel.Text = desvTime.ToString() + " ms";
+            this.rtMaxLabel.Text = maxTime.ToString() + " ms";
+        }
+
+        private void UpdateTurnaroundTime(int turnaroundTime)
+        {
+            this.turnaroundTimes.Add(turnaroundTime);
+
+            int minTime = turnaroundTime;
+            int avgTime = 0;
+            int maxTime = turnaroundTime;
+            int desvTime = 0;
+
+            foreach (int time in this.turnaroundTimes) {
+                avgTime += time;
+
+                if (time < minTime)
+                {
+                    minTime = time;
+                }
+
+                if (time > maxTime)
+                {
+                    maxTime = time;
+                }
+            }
+
+            avgTime /= this.turnaroundTimes.Count;
+            foreach (int time in this.turnaroundTimes)
+            {
+                desvTime += (int) Math.Pow(time - avgTime, 2);
+            }
+            desvTime = (int) Math.Sqrt(desvTime / this.turnaroundTimes.Count);
+
+            // Actualización de la interfaz
+            this.taMinLabel.Text = minTime.ToString() + " ms";
+            this.taAvgLabel.Text = avgTime.ToString() + " ms";
+            this.taDesvLabel.Text = desvTime.ToString() + " ms";
+            this.taMaxLabel.Text = maxTime.ToString() + " ms";
+        }
+
+        private void UpdateCpuTime()
+        {
+            this.cpuTimeLabel.Text = this.actualTime.ToString() + " ms";
+            this.labelIdleCpu.Text = this.idleTime.ToString() + " ms";
+            this.cpuBussyLabel.Text = (this.actualTime - this.idleTime).ToString() + " ms";
+            
+        }
+
+        delegate void UpdateProcessInChartCallback(Process p, Color c);
+        delegate void UpdateResponseTimeCallback(int responseTime);
+        delegate void UpdateTurnaroundTimeCallback(int responseTime);
+        delegate void UpdateCpuTimeCallback();
+
+        private void btnRestartProcesses_Click(object sender, EventArgs e)
+        {
+            this.KillProcesses();
+            this.GenerateProcesses();
+            this.btnStart.Enabled = true;
+
+            // Limpiar Listas de tiempos
+            this.responseTimes.Clear();
+            this.turnaroundTimes.Clear();
+
+            // Limpiar todas las etiquetas
+            this.taMinLabel.Text = "0 ms";
+            this.taAvgLabel.Text = "0 ms";
+            this.taDesvLabel.Text = "0 ms";
+            this.taMaxLabel.Text = "0 ms";
+            this.rtMinLabel.Text = "0 ms";
+            this.rtDesvLabel.Text = "0 ms";
+            this.rtAvgLabel.Text = "0 ms";
+            this.rtMaxLabel.Text = "0 ms";
+            this.cpuTimeLabel.Text = "0 ms";
+            this.labelIdleCpu.Text = "0 ms";
+            this.cpuBussyLabel.Text = "0 ms";
+        }
+    }
+}
