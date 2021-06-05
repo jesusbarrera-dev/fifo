139bd550381c98b1eb8461861c6fc7d6a8abab8b
Saulson <saulson.96@gmail.com>
2021-06-05 18:08:51 -0500

    Se agrega directorio para la practica de SJF

---
+++ SJF/Process.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica
+{
+    class Process
+    {
+        public int ID { get; set; }
+        public int ArrivalTime { get; set; }
+        public int ExecutionTime { get; set; }
+        public int ExecutedTime { get; set; }
+        public int RemaingTime
+        {
+            get { return this.ExecutionTime - this.ExecutedTime; }
+            private set { }
+        }
+    }
+}
